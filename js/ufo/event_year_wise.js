fb97d5222c7046b013d4497b7f8e83a965db8406
meabhishkkumar <kumar.abhishek@berkeley.edu>
2016-04-10 23:01:20 +0530

    adding area chart for ufo reporting year wise

--- js/ufo/event_year_wise.js
+++ js/ufo/event_year_wise.js
@@ -1,7 +1,7 @@
 (function () {
 
   // Set the dimensions of the canvas / graph
-  var margin = {top: 30, right: 20, bottom: 30, left: 50},
+  var margin = {top: 30, right: 20, bottom: 40, left: 70},
       width = 600 - margin.left - margin.right,
       height = 270 - margin.top - margin.bottom;
 
@@ -19,6 +19,11 @@
   var yAxis = d3.svg.axis().scale(y)
       .orient("left").ticks(5);
 
+  // for area
+  var area = d3.svg.area()
+            .x(function(d) { return x(d.year); })
+            .y0(height)
+            .y1(function(d) { return y(d.records); });
   // Define the line
   var valueline = d3.svg.line()
       .x(function(d) { return x(d.year); })
@@ -45,6 +50,15 @@
       x.domain(d3.extent(data, function(d) { return d.year; }));
       y.domain([0, d3.max(data, function(d) { return d.records; })]);
 
+
+
+      // for area
+      svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area);
+
+
       // Add the valueline path.
       svg.append("path")
           .attr("class", "line")
@@ -56,10 +70,35 @@
           .attr("transform", "translate(0," + height + ")")
           .call(xAxis);
 
+      // Add the X Axis Label
+      svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom )
+        .style("text-anchor", "middle")
+        .text("Event Year");
+
       // Add the Y Axis
       svg.append("g")
           .attr("class", "y axis")
           .call(yAxis);
+
+      // Add the Y-Axis Label
+      svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", (0 - margin.left))
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("No. of Reportings");
+
+      // Add the title
+      svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text("UFO Reportings - Year Wise");
 
   });
  }
